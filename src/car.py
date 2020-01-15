10df4d206a1f426023264a7e5ae7b7e37b259f2f
init0 <hunterbroadby@gmail.com>
2020-01-15 22:22:20 +0100

    works somewhat

--- src/car.py
+++ src/car.py
@@ -2,13 +2,15 @@ import pygame
 
 
 class Car(pygame.sprite.Sprite):
-    WIDTH = 40
-    HEIGHT = 60
+    WIDTH = 10
+    HEIGHT = 15
     COLOR = 234
 
     FRICTION = 0.05
     ACCELERATION = 1.5
-    STEERING = 0.1
+    MAX_ACCEL = 5
+    MAX_SPEED = 5
+    STEERING = 3
 
     def __init__(self, *groups):
         super().__init__(*groups)
@@ -19,24 +21,26 @@ class Car(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=(100, 100))
         self.accel = pygame.Vector2(0, 0)
         self.speed = pygame.Vector2(0, 0)
-        self.direction = pygame.Vector2(1, 0)
+        self.direction = self.original_dir = pygame.Vector2(1, 0)
         self.angle = 0
 
     def accelerate(self, f_or_b):
         self.accel += f_or_b*self.direction*Car.ACCELERATION
 
     def steer(self, r_or_l):
-        self.angle += 1 #(self.angle+r_or_l*Car.STEERING)%360
-        self.direction.rotate_ip(self.angle)
+        self.angle += r_or_l*Car.STEERING
+        self.angle %= 360
+        self.direction = self.original_dir.rotate(-self.angle)
         self.image = pygame.transform.rotate(self.original, self.angle)
         self.rect = self.image.get_rect(center=self.rect.center)
 
     def update(self, *args, **kwargs):
-        print(self.direction, self.angle)
+        print(self.speed, self.accel)
 
         if self.speed.length() != 0:
             friction = self.speed*-Car.FRICTION
             self.accel += friction.normalize()
+            self.speed = self.speed.normalize()*max(-Car.MAX_SPEED, min(self.speed.length(), Car.MAX_SPEED))
 
         self.speed += self.accel
         self.accel *= 0
